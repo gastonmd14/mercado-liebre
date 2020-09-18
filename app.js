5e545f32e6fc88aa5373f3fab83eacab35aa1979
Dominguez Gaston <gastonmd14@gmail.com>
2020-09-18 12:28:12 -0300

    se actualizo app, index, styles

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-const express = require ('express');
+const express = require('express');
+const app = express();
 const fs = require('fs');
 const path = require('path');
-const app = express();
 
-app.listen(3000, () => console.log('Server port Running on port 3000'));
+app.listen(3000, () => console.log('Server Running on Port 3000'));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     let file = path.resolve('index.html');
     res.sendFile(file);
 });
 
 app.get('*', function(req, res) {
-	if (req.url.endsWith('css')) {
-		let file = path.resolve('.' + req.url);
-	    return res.sendFile(file);
-	};
-	let image = ['jpg', 'jpeg', 'gif', 'png', 'svg'];
-	let ext = req.url.split('.')[1];
-	if (image.includes(ext)) {
-	    let file = path.resolve('.' + req.url);
-	    return res.sendFile(file);
-	};
+	if (req.url.includes('.')) {
+		let file = path.resolve('.' + req.url)
+		return res.sendFile(file)
+	}
+	res.send('Not found');
 });
-
 
